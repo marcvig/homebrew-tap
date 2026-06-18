3f890764580073e6a0214f8caf7598f66ce3877d
marcvig <marc@vigod.com>
2026-06-18 17:51:47 -0400

    orbit-launcher 0.3.4 (build 203)

--- Casks/orbit-launcher.rb
+++ Casks/orbit-launcher.rb
@@ -1,6 +1,6 @@
 cask "orbit-launcher" do
-  version "0.3.3,194"
-  sha256 "5b3b18af99b04711bb893a4853614a483df3fc69ba652482d33bd2f1d6a9e9f1"
+  version "0.3.4,203"
+  sha256 "6a6613f40f4679984518ac6a392f8c5bfee3247a1ce47a90feb43a13d0fcac61"
 
   url "https://get.orbitlauncher.app/Orbit%20Launcher-#{version.csv.first}-#{version.csv.second}.dmg"
   name "Orbit Launcher"
